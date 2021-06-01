4cc15ad615823c57a0c7e6aa65b032b9a16d70d4
Pedro Peral <pep.90@hotmail.com>
2021-05-31 22:03:09 -0300

    feat: add cards to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,25 @@
   <div class="home">
     <BlogPost :post="welcomeScreen"/>
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
   
 </template>
 
 <script>
 import BlogPost from "../componentes/BlogPost";
+import BlogCards from "../componentes/BlogCard";
 
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCards },
   data() {
     return {
        welcomeScreen: {
@@ -30,9 +39,46 @@ export default {
            title: "Blog 2 title",
            blogHTML: "Blog 2 filter post title",
            blogCoverPhoto: "designed-for-everyone"
+        },
+        {
+           title: "Blog 3 title",
+           blogHTML: "Blog 3 filter post title",
+           blogCoverPhoto: "designed-for-everyone"
         }
-      ]
+      ],
+
+      sampleBlogCards: [
+      {
+           blogTitle: "Blog 4 title",
+           blogCoverPhoto: "stock-1"
+        },
+        {
+           blogTitle: "Blog 5 title",
+           blogCoverPhoto: "stock-2"
+        },
+        {
+           blogTitle: "Blog 6 title",
+           blogCoverPhoto: "stock-3"
+        },
+        {
+           blogTitle: "Blog 7 title",
+           blogCoverPhoto: "stock-4",
+           blogDate: "May 1, 2021"
+        }
+      ]           
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+
+  .blog-card-wrap {
+    
+    h3 {
+      font-weight: 300;
+      font-size: 28px;
+      margin-bottom: 32px;
+    }
+  }
+</style>
